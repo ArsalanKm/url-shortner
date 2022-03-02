518a32e3b7e031cecf8351366bc273daf2aec349
ArsalanKm <arsalankarimzadeh1@gmail.com>
2022-03-02 19:09:33 +0330

    helpers added

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -2,18 +2,44 @@ package routes
 
 import (
 	"time"
+
+	"github.com/ArsalanKm/url-shortner/helpers"
+	"github.com/asaskevich/govalidator"
+	"github.com/gofiber/fiber/v2"
 )
 
 type request struct {
-	URL         		string        `json:"url"`
-	CustomShort 		string        `json:"short"`
-	Expiry      		time.Duration `json:"expiry"`
+	URL         string        `json:"url"`
+	CustomShort string        `json:"short"`
+	Expiry      time.Duration `json:"expiry"`
 }
 
 type response struct {
-	URL            		string `json:"url"`
-	CustomShort    		string `json:"custom_short"`
-	Expiry         		string `json:"expiry"`
-	XrateRemaining 		string `json:"rate_remaining"`
-	XrateLimitRest 		string `json:"rate_limit_rest"`
+	URL            string `json:"url"`
+	CustomShort    string `json:"custom_short"`
+	Expiry         string `json:"expiry"`
+	XrateRemaining string `json:"rate_remaining"`
+	XrateLimitRest string `json:"rate_limit_rest"`
+}
+
+func ShortenURL(ctx *fiber.Ctx) error {
+	body := new(request)
+	if err := ctx.BodyParser(&body); err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Can not parse json"})
+	}
+	// implement rate limiting
+
+	// check if the input url is actual url
+	if !govalidator.IsURL(body.URL) {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "bad url"})
+	}
+
+	// check for domain error
+	if !helpers.RemoveDomainError(body.URL) {
+		return ctx.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error": "service unavailable"})
+	}
+
+	// enforce https,SSl
+
+	body.URL = helpers.EnforceHttp(body.URL)
 }
